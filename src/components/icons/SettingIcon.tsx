b45b8846519eed71966177bbcc59f4cabfb33a54
hyemin <hm_stom@naver.com>
2022-04-09 18:41:39 +0900

    Fix : SVG icon move

---
+++ src/components/icons/SettingIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SettingIcon = () => {
+  return (
+    <svg
+      className="h-5 w-5"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      />
+    </svg>
+  );
+};
+
+export default SettingIcon;
